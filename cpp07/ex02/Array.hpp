182d8a2861391501a070c958fa11c0c86e29039e
esrabudakk <esra_b1644@outlook.com>
2023-01-21 16:50:20 +0300

    cpp07 100%

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,16 +6,16 @@ template <typename T>
 class Array
 {
 private:
-	T *arr;
+    T *arr;
 public:
-	unsigned int len;
-	Array(void);
-	Array(const unsigned int &n);
-	Array(const Array<T> &to_copy);
-	Array& operator=(const Array<T> &to_copy);
-	T& operator[](int idx) const;
-	unsigned int	size() const;
-	~Array();
+    unsigned int len;
+    Array(void);
+    Array(const unsigned int &n);
+    Array(const Array<T> &to_copy);
+    Array& operator=(const Array<T> &to_copy);
+    T& operator[](int idx) const;
+    unsigned int	size() const;
+    ~Array();
     class OutOfRange : public std::exception {
     public:
         const char *what() const throw() {
